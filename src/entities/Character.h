4c448d767b09d752f3e54c5a759b8d79a2a96a7f
Chris Ullyott <chris@monkdevelopment.com>
2017-11-29 17:48:24 -0800

    Remove unused character health

--- src/entities/Character.h
+++ src/entities/Character.h
@@ -3,19 +3,13 @@ using namespace std;
 class Character
 {
     public:
-        // Constants.
-        static const int MIN_HEALTH = 0;
-        static const int MAX_HEALTH = 100;
-
         // Properties.
         string name;
-        int health;
 
         // Constructor.
         Character()
         {
             this->name = "Player 1";
-            this->health = MAX_HEALTH;
         }
 
         Character& setName(string name)
@@ -28,32 +22,5 @@ class Character
         string getName()
         {
             return this->name;
-        }
-
-        Character& addHealth(int points)
-        {
-            if ((this->health + points) > MAX_HEALTH) {
-                this->health = MAX_HEALTH;
-            } else {
-                this->health += points;
-            }
-
-            return *this;
-        }
-
-        Character& subtractHealth(int points)
-        {
-            if ((this->health - points) < MIN_HEALTH) {
-                this->health = MIN_HEALTH;
-            } else {
-                this->health -= points;
-            }
-
-            return *this;
-        }
-
-        int getHealth()
-        {
-            return this->health;
         }
 };
